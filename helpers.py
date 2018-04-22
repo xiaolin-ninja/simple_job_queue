eddacc2d5b9bb253e43f07f28c309da98fe50ace
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-04-22 16:22:38 -0700

    add comments

--- helpers.py
+++ helpers.py
@@ -8,6 +8,7 @@ def process_job():
 	"""processes url web scraping request
 	If process has been done before, get result from database
 	Otherwise, process and add data to database"""
+
 	r = redis.StrictRedis()
 
 	curr_job = r.blpop('job_queue', 0)[1]
@@ -16,23 +17,21 @@ def process_job():
 	url = r.hget('urls', curr_job).decode("utf-8")
 	print('Current URL:', url)
 
-	# if this url has not been requested before, the query is falsy
+	# if this url has not been requested before/is not in the db
 	if not Site.query.filter_by(url=url).first():
 		# fetches url page source
 		html = str(get_html(url))
 		print('Successfully retrieved HTML')
-		# add to database
+		# add results to database
 		db.session.add(Site(url=url, html=html))
 		db.session.commit()
 		print('Added to database')
-	# complete job
+	# update job status
 	r.hset('status', curr_job, 'complete')
 	print('Job', curr_job, 'Completed')
 	return
 
 def get_html(url):
-	"""scrapes html page source of url"""
-	# declaring this helper function isn't necessary for now, 
-	# but if we want to further parse the page then it is useful
+	"""Fetches html page source of url"""
 	html = urllib.request.urlopen(url).read()
 	return html
--- server.py
+++ server.py
@@ -12,43 +12,46 @@ app = Flask(__name__)
 r = redis.StrictRedis()
 r.set('curr_id', 0)
 
-# If there is work in the queue, do it (on app start)
 
 # based on the example, I'm going to assume the user must submit
-# the url as 'www.site.com' without the http:// header
+# the url as 'www.site.com' without protocol headers, 
 @app.route("/add_job/<site>", methods=['POST'])
 def add_task(site):
     """add job to the queue, initiate web scraping process"""
-    # user passes URL to route
-    print('user input:', site)
+
     # I chose to use redis, so the queue could be accessed by helper functions,
     # since Flask is stateless across processes
     r = redis.StrictRedis()
     # increment job id counter
     job_id = r.incr('curr_id')
-    # add http header
+    # add http header, assume all jobs will be http:// for the sake of simplicity
     url = 'http://' + site
+    # redis hash to keep track of jobs
     r.hset('urls', job_id, url)
-    # add new job to queue
+    # redis hash to keep track of job statuses
     r.hset('status', job_id, 'processing')
+    # redis list job queue
     r.rpush('job_queue', job_id)
 
+    # start processing job in the background
     p = multiprocessing.Process(target=process_job)
     p.start()
 
+    # returns job ID to user
     return 'job ID:{}'.format(job_id)
 
 @app.route("/status/<job_id>")
 def check_job(job_id):
     """check job status, if done, return page source"""
-    # if job is complete, return html
-    print('job_id:', job_id)
-    print('status:', r.hget('status', job_id))
+
+    # convert from byte to unicode
     if r.hget('status', job_id).decode("utf-8") == 'complete':
+        # if job status is done, retrieve url attached to job ID
         url = r.hget('urls', job_id).decode("utf-8")
-        print('URL:', url)
+        # query database of past successful scrapes
         return Site.query.filter_by(url=url).first().html
-    # else return processing message
+    
+    # if job not complete, return processing message
     return 'Processing'
 # ------------------------------------------------- #
 
